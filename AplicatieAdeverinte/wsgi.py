a86c8f9c4930ffbf00dbb857c2eb0f6d639801ea
AllanT12 <50295250+AllanT12@users.noreply.github.com>
2023-06-23 08:12:52 +0300

    prod2

--- AplicatieAdeverinte/wsgi.py
+++ AplicatieAdeverinte/wsgi.py
@@ -11,6 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AplicatieAdeverinte.settings')
+#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AplicatieAdeverinte.settings')
+settings_module = 'azure_project.production' if 'PRODUCTION' in os.environ else 'azure_project.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
